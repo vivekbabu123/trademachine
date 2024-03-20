bc1853fab32208cd165f82136d72e621ab7f4f2b
vivekbabu123 <rajkmr98752@gmail.com>
2024-03-20 17:55:40 +0530

    update

--- Fitn_quipment.php
+++ Fitn_quipment.php
@@ -1,11 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>All Fitness Equipment Sales & Service</title>
+    <!-- custom css link -->
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <!-- bootstrap css link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
+
 <body>
-    
+    <!-- header start -->
+    <?php include "header.php" ?>
+
+
+    <!-- header finish -->
+    <!-- hero services start -->
+    <section class="hero_services">
+        <div class="container">
+            <div class="row">
+                <div class="hero_titleservice text-center text-white">
+                    <h1>All Fitness Equipment Sales & Service
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- hero services finish -->
+
+
+
+
+
+    <!-- footer start -->
+
+    <?php include "footer.php"?>
+    <!-- footer finish -->
+
+
+
+
+
+
+
+
+    <!-- bootstrap js link -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+
 </body>
+
 </html>
--- header.php
+++ header.php
@@ -19,19 +19,24 @@
 <!-- header start -->
 <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top">
     <div class="container">
-        <a class="navbar-brand" href="./"><img src="./assets/img/fitness-hub.png" alt="img" class="img-fluid logo_menu"> </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand" href="./"><img src="./assets/img/fitness-hub.png" alt="img" class="img-fluid logo_menu">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0 nav_barlist">
                 <li class="nav-item dropdown active">
-                    <a class="nav-link text-dark nav_bar dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link text-dark nav_bar dropdown-toggle" href="#" id="navbarDropdownMenuLink"
+                        role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Service
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                         <li><a class="dropdown-item" href="./services.php">Our Service</a></li>
-                        <li><a class="dropdown-item" href="#">All Fitness Equipment Sales & Service </a></li>
+                        <li><a class="dropdown-item" href="./Fitn_quipment.php">All Fitness Equipment Sales & Service
+                            </a>
+                        </li>
                         <li><a class="dropdown-item" href="#">Elliptical Sales & Service</a></li>
                         <li><a class="dropdown-item" href="#">Preventive Maintenance</a></li>
                         <li><a class="dropdown-item" href="#">Outdoor Gym</a></li>
@@ -51,7 +56,8 @@
                     <a class="nav-link text-dark nav_bar" href="./location.php">LOCATIONS</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle text-dark nav_bar" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle text-dark nav_bar" href="#" id="navbarDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
                         INDUSTRIES
 
                     </a>
@@ -73,27 +79,31 @@
                     </a>
                 </li>
                 <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-        
-ABOUT
-          </a>
-          <ul class="dropdown-menu dropdown-menu-white" aria-labelledby="navbarDarkDropdownMenuLink">
-            <li><a class="dropdown-item" href="./aboutus.php">ABOUT US</a></li>
-            <li><a class="dropdown-item" href="#">CFET</a></li>
-            <li><a class="dropdown-item" href="#">FAQS</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-               
-                 
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarDarkDropdownMenuLink"
+                                role="button" data-bs-toggle="dropdown" aria-expanded="false">
+
+                                ABOUT
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-white" aria-labelledby="navbarDarkDropdownMenuLink">
+                                <li><a class="dropdown-item" href="./aboutus.php">ABOUT US</a></li>
+                                <li><a class="dropdown-item" href="#">CFET</a></li>
+                                <li><a class="dropdown-item" href="#">FAQS</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+
+
             </ul>
             <form class="d-flex nav_btn ">
 
-                <a class="btn shadow-none  ms-2" href="#" type="submit"> <span class="me-2"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;">
-                            <path d="M17.707 12.293a.999.999 0 0 0-1.414 0l-1.594 1.594c-.739-.22-2.118-.72-2.992-1.594s-1.374-2.253-1.594-2.992l1.594-1.594a.999.999 0 0 0 0-1.414l-4-4a.999.999 0 0 0-1.414 0L3.581 5.005c-.38.38-.594.902-.586 1.435.023 1.424.4 6.37 4.298 10.268s8.844 4.274 10.269 4.298h.028c.528 0 1.027-.208 1.405-.586l2.712-2.712a.999.999 0 0 0 0-1.414l-4-4.001zm-.127 6.712c-1.248-.021-5.518-.356-8.873-3.712-3.366-3.366-3.692-7.651-3.712-8.874L7 4.414 9.586 7 8.293 8.293a1 1 0 0 0-.272.912c.024.115.611 2.842 2.271 4.502s4.387 2.247 4.502 2.271a.991.991 0 0 0 .912-.271L17 14.414 19.586 17l-2.006 2.005z">
+                <a class="btn shadow-none  ms-2" href="#" type="submit"> <span class="me-2"><svg
+                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                            style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;">
+                            <path
+                                d="M17.707 12.293a.999.999 0 0 0-1.414 0l-1.594 1.594c-.739-.22-2.118-.72-2.992-1.594s-1.374-2.253-1.594-2.992l1.594-1.594a.999.999 0 0 0 0-1.414l-4-4a.999.999 0 0 0-1.414 0L3.581 5.005c-.38.38-.594.902-.586 1.435.023 1.424.4 6.37 4.298 10.268s8.844 4.274 10.269 4.298h.028c.528 0 1.027-.208 1.405-.586l2.712-2.712a.999.999 0 0 0 0-1.414l-4-4.001zm-.127 6.712c-1.248-.021-5.518-.356-8.873-3.712-3.366-3.366-3.692-7.651-3.712-8.874L7 4.414 9.586 7 8.293 8.293a1 1 0 0 0-.272.912c.024.115.611 2.842 2.271 4.502s4.387 2.247 4.502 2.271a.991.991 0 0 0 .912-.271L17 14.414 19.586 17l-2.006 2.005z">
                             </path>
                         </svg></span>7250644045</a>
             </form>
